e56359897dc1ac64006cd16f5b4ee8052f0b849a
VioletaRS26 <svioleta1002@gmail.com>
2023-02-09 21:30:27 -0300

    Finalização da lista

--- exercicio09.js
+++ exercicio09.js
@@ -1,6 +1,14 @@
-function ipv4_bits(n){
-    
-    return n.toString(5);
+function ipv4_bits(string){
+    let parte = string.split('.');
+    let result = '';
+    for (let i=0; i < parte.length; i++) {
+      let part = parseInt(parte[i]);
+      let bits = part.toString(2);
+      while (bits.length < 8) {
+        bits = '0' + bits;
+      }
+      result += bits;
+    }
+    return result;
 }
-
-console.log(ipv4_bits(192.168));
+console.log(ipv4_bits("192.168.0.1"));
